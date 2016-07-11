ef6e8af9c2f329eb00cb9ac1109aa726b015c6bb
m-wiesner <wiesner@jhu.edu>
2016-07-11 14:37:30 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'b3',
+  packages = ['b3'], # this must be the same as the name above
+  version = '0.1',
+  description = 'BCUBED extrinsic clustering metric',
+  author = 'Matthew Wiesner',
+  author_email = 'wiesner@jhu.edu',
+  url = 'https://github.com/m-wiesner/BCUBED',
+  download_url = 'https://github.com/m-wiesner/BCUBED/tarball/0.1', # I'll explain this in a second
+  keywords = ['clustering', 'bcubed', 'evaluation'], # arbitrary keywords
+  classifiers = [],
+)
