37530a8c50a8f73b641b44785ad5aea7e0131391
Matthew Wiesner <wiesner@jhu.edu>
2016-07-11 16:02:00 -0400

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from distutils.core import setup
+import pdb
+
+pdb.set_trace()
 setup(
   name = 'b3',
   packages = ['b3'], # this must be the same as the name above
@@ -7,6 +10,7 @@ setup(
   author = 'Matthew Wiesner',
   author_email = 'wiesner@jhu.edu',
   url = 'https://github.com/m-wiesner/BCUBED',
+  install_requires = ["numpy"]
   download_url = 'https://github.com/m-wiesner/BCUBED/tarball/0.1', # I'll explain this in a second
   keywords = ['clustering', 'bcubed', 'evaluation'], # arbitrary keywords
   classifiers = [],
